f0643a786adb179ab47bcba17245b83b32bc6e5e
Pham Tien <84020540+PhamVanTien1910@users.noreply.github.com>
2025-11-05 21:08:20 +0700

    implement gps service and add localtion feature (#11)

---
+++ Services/ShopManagementService/ShopManagement.Application/Dtos/GpsCoordinateDto.cs
@@ -0,0 +1,8 @@
+namespace ShopManagement.Application.Dtos
+{
+    public class GpsCoordinateDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
